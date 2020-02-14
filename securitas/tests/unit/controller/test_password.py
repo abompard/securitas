07b6cd2b07de8763bc3875a53f2858082eee0533
Aurélien Bompard <aurelien@bompard.org>
2020-02-14 16:23:12 +0100

    Allow users to set a new password when they forgot it
    This follows advice from #23.
    
    Fixes: #104
    Signed-off-by: Aurélien Bompard <aurelien@bompard.org>

--- securitas/tests/unit/controller/test_password.py
+++ securitas/tests/unit/controller/test_password.py
@@ -1,11 +1,29 @@
+import datetime
+import re
 from unittest import mock
 
+import jwt
 import pytest
 import python_freeipa
 from bs4 import BeautifulSoup
-from flask import get_flashed_messages
+from flask import get_flashed_messages, current_app
 
-from securitas import ipa_admin
+from securitas import ipa_admin, mailer
+from securitas.security.ipa import untouched_ipa_client
+from securitas.utility.password_reset import PasswordResetLock
+
+
+@pytest.fixture
+def token_for_dummy_user(dummy_user):
+    last_change = ipa_admin.user_show("dummy")["krblastpwdchange"]
+    return str(
+        jwt.encode(
+            {"username": "dummy", "last_change": last_change},
+            current_app.config["SECRET_KEY"],
+            algorithm="HS256",
+        ),
+        "ascii",
+    )
 
 
 def test_password_reset(client):
@@ -278,3 +296,182 @@ def test_password_changes(client, dummy_user, no_password_min_time):
     category, message = messages[0]
     assert message == "Your password has been changed"
     assert category == "success"
+
+
+def test_forgot_password_ask(client):
+    result = client.get('/forgot-password/ask')
+    assert result.status_code == 200
+
+
+@pytest.mark.vcr()
+def test_forgot_password_ask_post(client, dummy_user):
+    with mailer.record_messages() as outbox:
+        with mock.patch.object(PasswordResetLock, "store") as store:
+            result = client.post('/forgot-password/ask', data={"username": "dummy"})
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/"
+    # Confirmation message
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == (
+        "An email has been sent to your address with instructions on how to reset your password"
+    )
+    assert category == "success"
+    # Sent email
+    assert len(outbox) == 1
+    message = outbox[0]
+    assert message.subject == "Password reset procedure"
+    assert message.recipients == ["dummy@example.com"]
+    # Valid token
+    token_match = re.search(r"\?token=([^\s\"']+)", message.body)
+    assert token_match is not None
+    token = token_match.group(1)
+    token_data = jwt.decode(token, current_app.config["SECRET_KEY"])
+    assert token_data.get("username") == "dummy"
+    assert "last_change" in token_data
+    # Lock activated
+    store.assert_called_once()
+
+
+@pytest.mark.vcr()
+def test_forgot_password_ask_post_non_existant_user(client):
+    result = client.post('/forgot-password/ask', data={"username": "nosuchuser"})
+    assert result.status_code == 200
+    page = BeautifulSoup(result.data, 'html.parser')
+    username_input = page.select_one("input[name='username']")
+    assert username_input is not None
+    assert 'is-invalid' in username_input['class']
+    invalidfeedback = username_input.find_next('div', class_='invalid-feedback')
+    assert invalidfeedback.get_text(strip=True) == "User nosuchuser does not exist"
+
+
+@pytest.mark.vcr()
+def test_forgot_password_ask_no_smtp(client, dummy_user):
+    with mock.patch("securitas.controller.password.mailer") as mailer:
+        mailer.send.side_effect = ConnectionRefusedError
+        with mock.patch.object(PasswordResetLock, "store") as store:
+            with mock.patch("securitas.controller.password.app.logger") as logger:
+                result = client.post('/forgot-password/ask', data={"username": "dummy"})
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/"
+    mailer.send.assert_called_once()
+    # Lock untouched
+    store.assert_not_called()
+    # Error message
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "We could not send you an email, please retry later"
+    assert category == "danger"
+    # Log message
+    logger.error.assert_called_once()
+
+
+def test_forgot_password_ask_still_valid(client):
+    with mailer.record_messages() as outbox:
+        with mock.patch.object(PasswordResetLock, "valid_until") as valid_until:
+            valid_until.return_value = datetime.datetime.now() + datetime.timedelta(
+                minutes=2
+            )
+            result = client.post('/forgot-password/ask', data={"username": "dummy"})
+    # Error message
+    assert result.status_code == 200
+    page = BeautifulSoup(result.data, 'html.parser')
+    submit_button = page.select("button[type='submit']")[0]
+    form_errors = submit_button.find_previous("div", id="formerrors")
+    assert form_errors is not None
+    form_error = form_errors.find("div", class_="text-danger")
+    assert form_error is not None
+    assert form_error.get_text(strip=True).startswith(
+        "You have already requested a password reset, you need to wait "
+    )
+    # No sent email
+    assert len(outbox) == 0
+
+
+def test_forgot_password_change_no_token(client):
+    result = client.get('/forgot-password/change')
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/forgot-password/ask"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "No token provided, please request one."
+    assert category == "warning"
+
+
+def test_forgot_password_change_invalid_token(client):
+    result = client.get('/forgot-password/change?token=this-is-invalid')
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/forgot-password/ask"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "The token is invalid, please request a new one."
+    assert category == "warning"
+
+
+@pytest.mark.vcr()
+def test_forgot_password_change_not_active(client, token_for_dummy_user):
+    with mock.patch.multiple(
+        PasswordResetLock, valid_until=mock.DEFAULT, delete=mock.DEFAULT
+    ) as patches:
+        valid_until = patches["valid_until"]
+        valid_until.return_value = None
+        result = client.get(f'/forgot-password/change?token={token_for_dummy_user}')
+    patches["delete"].assert_called_once()
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/forgot-password/ask"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "The token has expired, please request a new one."
+    assert category == "warning"
+
+
+@pytest.mark.vcr()
+def test_forgot_password_change_too_old(client, token_for_dummy_user):
+    with mock.patch.multiple(
+        PasswordResetLock, valid_until=mock.DEFAULT, delete=mock.DEFAULT
+    ) as patches:
+        valid_until = patches["valid_until"]
+        valid_until.return_value = datetime.datetime.now() - datetime.timedelta(
+            minutes=1
+        )
+        result = client.get(f'/forgot-password/change?token={token_for_dummy_user}')
+    patches["delete"].assert_called_once()
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/forgot-password/ask"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == "The token has expired, please request a new one."
+    assert category == "warning"
+
+
+@pytest.mark.vcr()
+def test_forgot_password_change_recent_password_change(
+    client, dummy_user, token_for_dummy_user, no_password_min_time
+):
+    ipa_admin.group_add_member("dummy-group", users="dummy")
+    ipa = untouched_ipa_client(current_app)
+    ipa.change_password("dummy", "dummy_password", "dummy_password")
+    with mock.patch.multiple(
+        PasswordResetLock, valid_until=mock.DEFAULT, delete=mock.DEFAULT
+    ) as patches:
+        valid_until = patches["valid_until"]
+        valid_until.return_value = datetime.datetime.now() + datetime.timedelta(
+            minutes=2
+        )
+        result = client.get(f'/forgot-password/change?token={token_for_dummy_user}')
+    patches["delete"].assert_called_once()
+    assert result.status_code == 302
+    assert result.location == f"http://localhost/forgot-password/ask"
+    messages = get_flashed_messages(with_categories=True)
+    assert len(messages) == 1
+    category, message = messages[0]
+    assert message == (
+        "Your password has been changed since you requested this token, please request a new one."
+    )
+    assert category == "warning"
